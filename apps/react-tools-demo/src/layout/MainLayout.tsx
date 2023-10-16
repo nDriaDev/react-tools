4120b274601786da4bc33274d0a264536665d290
root <root@DESKTOP-T5TH464>
2023-10-16 10:08:19 +0200

    [FIX] responsive demo

--- apps/react-tools-demo/src/layout/MainLayout.tsx
+++ apps/react-tools-demo/src/layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Link, useLocation, useOutlet } from 'react-router-dom';
 import Logo from '../assets/github.svg';
 import React from '../assets/react-red.webp';
 import { COMPONENTS } from '../constants/components';
-import { Fragment, useRef } from 'react';
+import { Fragment, useCallback, useRef } from 'react';
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
 
 export default function MainLayout() {
@@ -10,41 +10,55 @@ export default function MainLayout() {
 	const location = useLocation()
 	const currentOutlet = useOutlet()
 	const nodeRef = useRef<HTMLDivElement>(null);
+	const navRef = useRef<HTMLUnknownElement>(null);
+
+	const openNav = useCallback(() => {
+		navRef.current && (navRef.current.style.width = "100%");
+	}, []);
+
+	const closeNav = useCallback(() => {
+		navRef.current && (navRef.current.style.width= "0");
+	}, []);
+
     return (
         <>
 			{
 				!["/", ""].includes(pathname) &&
-				<nav className='nav'>
-					<div className='title-container'>
-						<Link to="/" className='title'>
-							<img src={React} alt='react' className='img' />
-							<p className='text'>React Tools</p>
-						</Link>
-						<Link to="https://github.com/nDriaDev/react-tools">
-							<img src={Logo} className='img' alt='github'/>
-						</Link>
-					</div>
-					{
-						COMPONENTS.map((el, index) => (<Fragment key={index === 0 ? 'Hooks' : 'Utils'}>
-							<p
-								className='type'
-							>
-								{index === 0 ? 'Hooks' : 'Utils'}
-							</p>
-							{
-								el.map(name => (
-									<Link
-										key={name}
-										to={"/"+name}
-										className={pathname === ("/"+name) ? 'active' : ''}
-									>
-										{name}
-									</Link>
-								))
-							}
-						</Fragment>))
-					}
-				</nav>
+				<>
+					<button onClick={openNav} className='open-nav'>☰</button>
+					<nav ref={navRef}  className='nav'>
+						<button onClick={closeNav} className='btn-close'>X</button>
+						<div className='title-container'>
+							<Link to="/" className='title'>
+								<img src={React} alt='react' className='img' />
+								<p className='text'>React Tools</p>
+							</Link>
+							<Link to="https://github.com/nDriaDev/react-tools">
+								<img src={Logo} className='img' alt='github' />
+							</Link>
+						</div>
+						{
+							COMPONENTS.map((el, index) => (<Fragment key={index === 0 ? 'Hooks' : 'Utils'}>
+								<p
+									className='type'
+								>
+									{index === 0 ? 'Hooks' : 'Utils'}
+								</p>
+								{
+									el.map(name => (
+										<Link
+											key={name}
+											to={"/" + name}
+											className={pathname === ("/" + name) ? 'active' : ''}
+										>
+											{name}
+										</Link>
+									))
+								}
+							</Fragment>))
+						}
+					</nav>
+				</>
 			}
 			<div className='container'>
 				<SwitchTransition>
--- apps/react-tools-demo/src/markdown/UseStateGetter.md
+++ apps/react-tools-demo/src/markdown/UseStateGetter.md
@@ -53,7 +53,7 @@ export { UseStateGetter };
 > The component has:
 > - A _useStateGetter_ that receives an object, with three properties _id_, _name_, _eta_, and returns a tuple with _stateG_, _setterG_ and _getter_.
 > - A _useState_ that receives same object of useStateGetter and returns a tuple with _state_ and _setter_.
-> - Each property of state and stateG is rendered an Input component that renders the input and label fields and the _component's number of renders_.
+> - Each property of _state_ and _stateG_ is rendered an Input component that renders the input and label fields and the _component's number of renders_.
 > - A _onChangeGetter_ function made with useCallback to handle stateGetter input onChange, reading other values with getter function.
 > - A _onChange_ function made with useCallback to handle stater input onChange, reading other values by state variable.
 > The two functions onChange and onChange Getter update their respective _state_ every time they are executed. Since the _onChange_ function depends on the state, every time this changes it will be reevaluated and this will also trigger the rerender of the input components that have not undergone a change to their _value_ variable. The _onChangeGetter_ doesn't have this behavior: since the _getter_ function isn't reevaluated even if the _stateG_ changes, so the _onChangeGetter_ is never reevaluated and only the input component that has a change in the _value_ variable is rerendered.
