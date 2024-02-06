578d81b61dd332239cfa823d1c6ecc48b782b680
nDriaDev <andreacosentino.work@gmail.com>
2024-02-06 02:38:24 +0100

    [FIX] For ErrorBoundary

--- apps/react-tools-demo/src/pages/components/errorBoundary/ErrorBoundary.tsx
+++ apps/react-tools-demo/src/pages/components/errorBoundary/ErrorBoundary.tsx
@@ -1,7 +1,7 @@
 import { ErrorBoundary } from "../../../../../../packages/react-tools/src";
 
 /**
-The component uses _ErrorBoundary_ Component to wrap _Component_. _Component_ throw an error so _ErrorBoundary_ catch it and show the _fallback_ element passing to it _error_ and _retry_ function.
+The component uses _ErrorBoundary_ Component to wrap _Component_. _Component_ throw an error so _ErrorBoundary_ catch it and show the _fallback_ element passing to it _error_ and _retry_ function. [See demo](https://nDriaDev.io/react-tools/#/components/ErrorBoundary)
  */
 const Component = () => {
 	throw new Error("An error occurred.");
