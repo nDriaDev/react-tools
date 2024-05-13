8b16ae5fb7e8e0e25aeaba06feaad732995a53fc
nDriaDev <andreacosentino.work@gmail.com>
2024-05-14 00:47:08 +0200

    [FIX] docs

---
+++ docs/assets/useReducedMotion--RvqRNvC.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useMediaQuery-u_CI3fYa.js";const o=()=>e("(prefers-reduced-motion: reduce)").matches;export{o as u};
--- docs/assets/useRerender-GR08ZFNS.js
+++
@@ -1,1 +0,0 @@
-import{r as u}from"./index-7iLlK_Uu.js";function s(r){const e=u.useReducer(t=>t+1e-11,0);return r?e:e[1]}export{s as u};
