f7c767f165de8e53ac460669635a3efb325c2197
nDriaDev <andreacosentino.work@gmail.com>
2024-05-19 02:25:58 +0200

    [FIX] homePage

---
+++ docs/assets/UseNetwork-39neTicQ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-uMjCFCDe.js";import{u as s}from"./useNetwork-tO1DhWPm.js";import"./useSyncExternalStore-V-ukT-au.js";const c=()=>{const e=s();return r.jsx("div",{style:{textAlign:"center"},children:Object.keys(e).map(t=>r.jsxs("p",{children:[t,": ",JSON.stringify(e[t])]},t))})};export{c as UseNetwork};
